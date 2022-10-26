2441031cb9dd94e2c0819caba7ff034ede27abcd
Евгений Котвицкий <ekotvizky@gmail.com>
2022-10-26 12:36:47 +0300

    image binding

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -8,6 +8,33 @@ export class ImagesComponent implements OnInit {
 
   constructor() { }
 
+  private pointer: number = 0;
+
+  imgSrc = [
+    {src:'./assets/bootstrap.jpg', alt: 'bootstrap'},
+    {src:'./assets/materialize.png', alt: 'materialize'},
+    {src:'./assets/tailwind.png', alt: 'tailwind'},
+  ]
+
+  currentScr = './assets/bootstrap.jpg';
+
+  currentAlt = 'bootstrap'
+
+  private initImg(){
+    this.currentScr = this.imgSrc[this.pointer].src;
+    this.currentAlt = this.imgSrc[this.pointer].alt;
+  }
+
+  prevHandle(e:any) {
+    this.pointer = (this.pointer <= 0) ? this.imgSrc.length - 1 : this.pointer - 1;
+    this.initImg();
+  }
+
+  nextHandle(e:any) {
+    this.pointer = (this.pointer < this.imgSrc.length - 1)  ? this.pointer + 1 : 0;
+    this.initImg();
+  }
+
   ngOnInit(): void {
   }
 
